36ed6aa3e9afad42cb291dae93ce0f5ea4ddfc91
Arash Madadi <arashmd81@GMAIL.COM>
2022-08-18 17:37:37 +0430

    clean up and install needed package;
    clean the unnecessary default code
    and
    install bootstrap;

--- todo-list-app/src/main.js
+++ todo-list-app/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "@popperjs/core";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 createApp(App).use(router).mount("#app");
