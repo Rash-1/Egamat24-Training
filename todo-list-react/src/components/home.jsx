e493a57b3d2e3761a35c4e8a2b4833519112c8dc
Arash Madadi <arashmd81@GMAIL.COM>
2022-08-24 18:00:17 +0430

    Home component created

--- todo-list-react/src/components/home.jsx
+++ todo-list-react/src/components/home.jsx
@@ -1,9 +1,13 @@
 
 
 const Home = ()=>{
-    return <>
-        <h1>Home</h1>
-    </>
+    return (<>
+    <div style={{borderRadius:"20rem",padding:'10rem',marginTop:'5rem'}} className="shadow-lg text-center container mx-auto">
+        <h1>
+            Welcome To Your ToDO App
+        </h1>
+    </div>
+    </>)
 }
 
 export default Home;
