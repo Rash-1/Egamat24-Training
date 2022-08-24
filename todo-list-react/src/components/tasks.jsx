2d6fe37964f411ddd9f88722e0face09e5240cfa
Arash Madadi <arashmd81@GMAIL.COM>
2022-08-25 01:57:48 +0430

    Done Tasks Checkbox added

--- todo-list-react/src/components/tasks.jsx
+++ todo-list-react/src/components/tasks.jsx
@@ -1,20 +1,52 @@
 import TasksContext from "../context/tasks";
-import {useContext,useState} from "react";
+import {useContext, useState, useRef, useEffect} from "react";
 import Task from "./task"
 
 
 const Tasks = ()=>{
-    const tasksContext = useContext(TasksContext)
-    const [label,setLabel] = useState('Task')
-    return( <>
-        <h1 className="text-center">{label}</h1>
-        <div className="tasks">
-            {tasksContext.Tasks.map((task,index)=>(
-                <Task task={task} key={index}/>
-            ))}
-        </div>
-    </>)
+    const tasksContext = useContext(TasksContext);
+    const [label,setLabel] = useState('All Task');
+    const [doneTasks,setDoneTasks] = useState([])
+    const [checked,setChecked] = useState(true);
+    return(
+        <>
+            <h1 className="text-center">{label}</h1>
+            <div className="text-center">
+                <label htmlFor="checkBox">Show Done Tasks</label>
+                <input checked={!checked} onChange={checkBoxChange}  id="checkBox" type="checkbox" className="form-check-input"/>
+            </div>
+            {
+                !checked ?
+                    (
+                        <div className="tasks">
+                            {doneTasks.map((task,index)=>(
+                                <Task task={task} key={index}/>
+                            ))}
+                        </div>
+                    )
+                    :
+                    (
+                        <div className="tasks">
+                            {tasksContext.Tasks.map((task,index)=>(
+                                <Task task={task} key={index}/>
+                            ))}
+                        </div>
+                    )
+            }
+        </>
+    )
 
+    function checkBoxChange(){
+        setChecked(!checked)
+        if (checked){
+            setLabel('Done Tasks')
+            let filteredTasks = tasksContext.Tasks.filter((task)=>{
+                return task.status === "Done"
+            })
+            setDoneTasks(filteredTasks)
+        }
+        console.log(checked)
+    }
 }
 
 export default Tasks;
