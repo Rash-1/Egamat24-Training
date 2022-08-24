0043ad93f7a5a33c6d12c323068d083824871157
Arash Madadi <arashmd81@GMAIL.COM>
2022-08-24 17:44:21 +0430

    Navbar created

--- todo-list-react/src/components/navBar.jsx
+++ todo-list-react/src/components/navBar.jsx
@@ -1,9 +1,32 @@
-
+import {NavLink, Link } from "react-router-dom";
 
 const NavBar = ()=>{
-    return <>
-    <h1>navbar</h1>
-    </>
+    return (<>
+    <nav className="navbar px-2 d-flex justify-content-start border-bottom border-secondary border-opacity-50 ">
+        <div className="me-3">
+            <p className="navbar-brand">Todo App</p>
+        </div>
+        <div className="flex-row d-flex justify-content-between ms-2">
+            <NavLink style={({isActive})=>{
+                return{
+                    color:isActive ? '#1565c0' : 'black'
+                }
+            }} className="nav-link mx-3" to="/home">Home</NavLink>
+            <NavLink style={({isActive})=>{
+                return{
+                    color:isActive ? '#1565c0' : 'black'
+                }
+            }} className="nav-link mx-3" to="/tasks">Tasks</NavLink>
+            <NavLink style={({isActive})=>{
+                return{
+                    color:isActive ? '#1565c0' : 'black'
+                }
+            }} className="nav-link mx-3" to="/create-task">Create New Task</NavLink>
+        </div>
+    </nav>
+
+
+    </>)
 }
 
 export default NavBar;
