7c408e621c5f620f8accf57b07f883fa8bd990c6
Arash Madadi <arashmd81@GMAIL.COM>
2022-09-07 12:23:31 +0430

    Update Client.php
    providedServices function added for getting the services that client buy them

--- installment-sales/app/Models/Client.php
+++ installment-sales/app/Models/Client.php
@@ -3,11 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+
 class Client extends Authenticatable
 {
     use HasFactory;
+
     protected $table = 'clients';
     protected $fillable = ['firstName', 'lastName', 'username', 'password'];
+
+
+    public function providedServices()
+    {
+        return $this->belongsToMany(ProvidedService::class, 'providedServicesReports');
+
+    }
 }
