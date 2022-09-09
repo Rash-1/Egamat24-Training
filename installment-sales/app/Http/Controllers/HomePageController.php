410dc8ad06e570bbf01fc5f326ef9f71d1608d33
Arash Madadi <arashmd81@GMAIL.COM>
2022-09-09 17:31:59 +0430

    Update HomePageController.php

--- installment-sales/app/Http/Controllers/HomePageController.php
+++ installment-sales/app/Http/Controllers/HomePageController.php
@@ -14,10 +14,17 @@ class HomePageController extends Controller
         return view('home', ['services' => $services, 'work_fields' => $work_fields]);
     }
 
-    function work_field_services(WorkField $workField)
+    public function work_field_services(WorkField $workField)
     {
         $services = Service::where('work_field_id', $workField->id)->get();
         $work_fields = WorkField::all();
-        return view('client/categoryServices',['services'=>$services,'work_fields'=>$work_fields]);
+        return view('client/categoryServices', ['services' => $services, 'work_fields' => $work_fields]);
     }
+
+    public function show_service_payment_conditions(Service $service)
+    {
+        $payment_conditions = $service->paymentConditions()->get();
+        return view('client/paymentConditions', ['service' => $service, 'payment_conditions' => $payment_conditions]);
+    }
+
 }
